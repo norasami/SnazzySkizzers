67702818bc1bc0427fe834f9c1c1dfcd16113ae5
Nora Luck-Jennings <1018949@student.sae.edu.au>
2021-02-17 13:20:42 +1100

    Programmed Skizzor movement and animation
    Wrote scripts and used Input System and Animator to make Skizzors sprite move around using analogue stick and play cutting animation on button press

--- SnazzySkizzers/Assets/Skizzors.cs
+++ SnazzySkizzers/Assets/Skizzors.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Skizzors : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerControls controls;
+    Animator anim;
+    Vector2 move;
+
+    void Awake()
     {
-        
+        controls = new PlayerControls();
+
+        controls.Gameplay.Cut.performed += ctx => Cut();
+
+        controls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += ctx => move = Vector2.zero;
+
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    void Cut()
+    {
+        if (anim != null)
+        {
+            anim.SetTrigger("UseSkizzors");
+        }
+    }
+
     void Update()
     {
-        
+        Vector2 m = new Vector2(move.x, move.y) * Time.deltaTime;
+        transform.Translate(m, Space.World);
+    }
+
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
     }
 }
